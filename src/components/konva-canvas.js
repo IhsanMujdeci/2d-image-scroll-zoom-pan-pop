3ed72cb801005d219775ca19b7c3e32773b6a1c9
ihsan <ihsan@driveyello.com>
2018-11-25 12:57:22 +1100

    first push, better have history of changes is something goes wrong

---
+++ src/components/konva-canvas.js
@@ -0,0 +1,273 @@
+import React, { Component } from 'react';
+import { Stage, Layer, Image } from 'react-konva';
+//import Konva from 'konva'
+
+const imagePath = "/images/tiled/";
+const images = [
+    [
+        [imagePath+'/0/0/0.jpg']
+    ],
+    [
+        [imagePath+'/1/0/0.jpg', imagePath+'/1/0/1.jpg'],
+        [imagePath+'/1/1/0.jpg', imagePath+'/1/1/1.jpg']
+    ],
+    [
+        [imagePath+'/2/0/0.jpg', imagePath+'/2/0/1.jpg', imagePath+'/2/0/2.jpg',imagePath+'/2/0/3.jpg'],
+        [imagePath+'/2/1/0.jpg', imagePath+'/2/1/1.jpg', imagePath+'/2/1/2.jpg',imagePath+'/2/1/3.jpg'],
+        [imagePath+'/2/2/0.jpg', imagePath+'/2/2/1.jpg', imagePath+'/2/2/2.jpg',imagePath+'/2/2/3.jpg'],
+        [imagePath+'/2/3/0.jpg', imagePath+'/2/3/1.jpg', imagePath+'/2/3/2.jpg',imagePath+'/2/3/3.jpg']
+    ],
+    [
+        [
+            imagePath+'/3/0/0.jpg', imagePath+'/3/0/1.jpg', imagePath+'/3/0/2.jpg',imagePath+'/3/0/3.jpg',
+            imagePath+'/3/0/4.jpg', imagePath+'/3/0/5.jpg', imagePath+'/3/0/5.jpg',imagePath+'/3/0/6.jpg'
+        ],
+        [
+            imagePath+'/3/1/0.jpg', imagePath+'/3/1/1.jpg', imagePath+'/3/1/2.jpg',imagePath+'/3/1/3.jpg',
+            imagePath+'/3/1/4.jpg', imagePath+'/3/1/5.jpg', imagePath+'/3/1/5.jpg',imagePath+'/3/1/6.jpg'
+        ],
+        [
+            imagePath+'/3/2/0.jpg', imagePath+'/3/2/1.jpg', imagePath+'/3/2/2.jpg',imagePath+'/3/2/3.jpg',
+            imagePath+'/3/2/4.jpg', imagePath+'/3/2/5.jpg', imagePath+'/3/2/5.jpg',imagePath+'/3/2/6.jpg'
+        ],
+        [
+            imagePath+'/3/3/0.jpg', imagePath+'/3/3/1.jpg', imagePath+'/3/3/2.jpg',imagePath+'/3/3/3.jpg',
+            imagePath+'/3/3/4.jpg', imagePath+'/3/3/5.jpg', imagePath+'/3/3/5.jpg',imagePath+'/3/3/6.jpg'
+        ],
+        [
+            imagePath+'/3/4/0.jpg', imagePath+'/3/4/1.jpg', imagePath+'/3/4/2.jpg',imagePath+'/3/4/3.jpg',
+            imagePath+'/3/4/4.jpg', imagePath+'/3/4/5.jpg', imagePath+'/3/4/5.jpg',imagePath+'/3/4/6.jpg'
+        ],
+        [
+            imagePath+'/3/5/0.jpg', imagePath+'/3/5/1.jpg', imagePath+'/3/5/2.jpg',imagePath+'/3/5/3.jpg',
+            imagePath+'/3/5/4.jpg', imagePath+'/3/5/5.jpg', imagePath+'/3/5/5.jpg',imagePath+'/3/5/6.jpg'
+        ],
+        [
+            imagePath+'/3/6/0.jpg', imagePath+'/3/6/1.jpg', imagePath+'/3/6/2.jpg',imagePath+'/3/6/3.jpg',
+            imagePath+'/3/6/4.jpg', imagePath+'/3/6/5.jpg', imagePath+'/3/6/5.jpg',imagePath+'/3/6/6.jpg'
+        ],
+        [
+            imagePath+'/3/7/0.jpg', imagePath+'/3/7/1.jpg', imagePath+'/3/7/2.jpg',imagePath+'/3/7/3.jpg',
+            imagePath+'/3/7/4.jpg', imagePath+'/3/7/5.jpg', imagePath+'/3/7/5.jpg',imagePath+'/3/7/6.jpg'
+        ]
+    ]
+];
+
+class CanvasComponent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            image: null,
+            imageGroup: images,
+            stage: this.refs.stage,
+            scaleBy: 1.1,
+            lastDist: 0,
+            scale: 1,
+            x:0,
+            y:0,
+            baseImageWidth:0,
+            baseImageHeight:0,
+            img0: null,
+            img1: null,
+            img2: null,
+            img3: null,
+            layer0Visible: true,
+            layer1Visible: false,
+        }
+    }
+    componentDidMount() {
+        this.updateCanvas();
+    }
+    updateCanvas(){
+        const image = new window.Image();
+
+        image.src = images[0][0];
+        image.onload = ()=> {
+
+
+            console.log(image.width)
+            console.log(image.height)
+            this.setState({
+                image:  image,
+                baseImageWidth: image.width,
+                baseImageHeight: image.hegith,
+            })
+        };
+
+        const img0 = new window.Image();
+        img0.src = images[1][0][0];
+        img0.onload = () => {
+            this.setState({
+                img0: img0
+            })
+        }
+
+        const img1 = new window.Image();
+        img1.src =  images[1][1][0];
+        img1.onload = () => {
+            this.setState({
+                img2: img2
+            })
+        };
+
+        const img2 = new window.Image();
+        img2.src = images[1][0][1];
+        img2.onload = () => {
+            this.setState({
+                img1: img1
+            })
+        };
+
+        const img3 = new window.Image();
+        img3.src =  images[1][1][1];
+        img3.onload = () => {
+            this.setState({
+                img3: img3
+            })
+        }
+    }
+    getDimension(layer){
+        return this.state.baseImageWidth/(layer+1)
+    }
+    wheel({evt}){
+        evt.preventDefault();
+
+        const stage = this.refs.stage;
+        const oldScale = this.state.scale;
+
+        const mousePointX = stage.getPointerPosition().x / oldScale - stage.x() / oldScale;
+        const mousePointY = stage.getPointerPosition().y / oldScale - stage.y() / oldScale;
+
+        const newScale = evt.deltaY < 0 ? oldScale * this.state.scaleBy : oldScale / this.state.scaleBy;
+
+        // we can refactor this to its own function
+        if(newScale > 2.1 && !this.state.layer1Visible){
+            this.setState({
+                layer0Visible: false,
+                layer1Visible: true,
+            })
+        }
+        if(newScale < 2.1 && !this.state.layer0Visible){
+            this.setState({
+                layer0Visible: true,
+                layer1Visible: false,
+            })
+        }
+
+        this.setState({
+            scale: newScale,
+            x: -(mousePointX - stage.getPointerPosition().x / newScale) * newScale,
+            y: -(mousePointY - stage.getPointerPosition().y / newScale) * newScale
+        });
+
+        stage.batchDraw();
+    }
+    render() {
+        return (
+            <>
+                <p>Zoom: {this.state.scale} {this.getDimension(1)}</p>
+                <Stage
+                    ref="stage"
+                    width={window.innerWidth}
+                    height={window.innerHeight}
+                    onWheel = {(e) => this.wheel(e)}
+                    draggable={true}
+                    scaleX={this.state.scale}
+                    scaleY={this.state.scale}
+                    x={this.state.x}
+                    y={this.state.y}
+                >
+
+
+                    <Layer ref="layer"
+                           width={this.state.baseImageWidth}
+                           height={this.state.baseImageHeight}
+                           visible={this.state.layer0Visible}
+                           listening={false}
+
+                    >
+                        <Image
+                            image={this.state.image}
+                            height={this.state.baseImageWidth}
+                            width={this.state.baseImageHeight}
+                            x={0}
+                            y={0}
+                            strokeEnabled={false}
+                            strokeScaleEnabled={false}
+                            shadowForStrokeEnabled={false}
+                            shadowEnabled={false}
+                            dashEnabled={false}
+                            listening={false}
+                            transformsEnabled={'position'}
+                        />
+                    </Layer>
+
+                    <Layer
+                        ref="layer"
+                        width={this.state.baseImageWidth}
+                        height={this.state.baseImageHeight}
+                        visible={this.state.layer1Visible}
+                    >
+                        <Image
+                            image={this.state.img0}
+                            height={this.getDimension(1)}
+                            width={this.getDimension(1)}
+                            x={0}
+                            y={0}
+                        />
+                        <Image
+                            image={this.state.img1}
+                            height={this.getDimension(1)}
+                            width={this.getDimension(1)}
+                            x={128}
+                            y={0}
+                        />
+                        <Image
+                            image={this.state.img2}
+                            height={this.getDimension(1)}
+                            width={this.getDimension(1)}
+                            x={0}
+                            y={128}
+                        />
+                        <Image
+                            image={this.state.img3}
+                            height={this.getDimension(1)}
+                            width={this.getDimension(1)}
+                            x={128}
+                            y={128}
+                        />
+                    </Layer>
+                </Stage>
+            </>
+        );
+    }
+    renderz() {
+        return (
+            <>
+                <p>Zoom: {this.state.scale}</p>
+                <Stage
+                    ref="stage"
+                    width={window.innerWidth}
+                    height={window.innerHeight}
+                    onWheel = {(e) => this.wheel(e)}
+                    draggable={true}
+                    scaleX={this.state.scale}
+                    scaleY={this.state.scale}
+                    x={this.state.x}
+                    y={this.state.y}
+                >
+                    {this.state.imageGroup.map(group =>
+                        <Layer>
+                            {group.map(image =>
+                                <Image/>
+                            )}
+                        </Layer>
+                    )}
+
+                </Stage>
+            </>
+        );
+    }
+}
+
+export default CanvasComponent
